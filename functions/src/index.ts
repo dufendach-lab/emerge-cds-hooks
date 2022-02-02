f157193c7f1c150a6be8c2538203e6ce26451d5b
Dufendach, Kevin <KevinDufendach@users.noreply.github.com>
2022-02-02 11:57:09 -0500

    update CDS service to send cds-services json

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import * as services from "./cds-services.json";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +8,9 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+export const cdsServices = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello CDS Service logs!", {structuredData: true});
+  response.send(services);
+});
+
